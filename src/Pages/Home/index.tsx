51eb85d972a9cdaec9a82f766ec72ad6b98dbfc7
kewensilva <kewen.macedo@gmail.com>
2023-01-13 22:05:55 -0300

    add styled components

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Content } from "./styles";
+
+export function Home(){
+    return(
+        <Container>
+            <Content>Olá Mundo!</Content>
+        </Container>
+    )
+}
---
+++ src/Pages/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+flex: 1;
+background-color: #ddd;
+justify-content: center;
+align-items: center;
+align-content: center;
+`;
+
+export const Content = styled.Text`
+color: #000;
+font-weight: bold;
+font-size: 20px;
+`;
